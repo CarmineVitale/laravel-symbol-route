ab9b444800657a6f37ddec69842b58f2295bde7a
CarmineV <carmine.vitale90@gmail.com>
2020-06-10 15:30:25 +0200

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel</title>
+</head>
+<body>
+    <header>
+        <h1>My First Laravel Layout</h1>
+        <ul class="links">
+            <li> <a href="#">News</a> </li>
+            <li> <a href="#">Contacts</a> </li>
+            <li> <a href="#">About</a> </li>
+        </ul>
+    </header>
+    <main>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi dolorum voluptatem vitae laboriosam eum quidem quas quos possimus, expedita tenetur, sint illo rem quae voluptates ipsam eligendi architecto earum illum?</p>
+    </main>
+    <footer>
+        <p>Made with love by Carmine Vitale - Boolean Class 12 &copy;</p>
+    </footer>
+</body>
+</html>
